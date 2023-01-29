eaf1e9f68a2b131da3b6bc8d8208355bf47b6742
GramBezEndu <33137304+GramBezEndu@users.noreply.github.com>
2023-01-29 13:20:23 +0100

    Hex drawing: combining meshes

--- Assets/Scripts/HexInfo.cs
+++ Assets/Scripts/HexInfo.cs
@@ -2,56 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexInfo : MonoBehaviour
+public class HexInfo
 {
-    [SerializeField]
-    private Texture texture;
+    public HexChunk Chunk { get; set; }
 
-    private Vector3[] vertices;
+    public Mesh Mesh { get; private set; }
 
-    private Vector2[] uv;
+    public Vector3 LocalPosition { get; set; }
 
-    private int[] triangles;
-
-    private void Start()
+    public void InitializeMesh()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(-1f , -.5f),
-            new Vector3(-1f, .5f),
-            new Vector3(0f, 1f),
-            new Vector3(1f, .5f),
-            new Vector3(1f, -.5f),
-            new Vector3(0f, -1f)
-        };
-
-        triangles = new int[]
-        {
-            1, 5, 0,
-            1, 4, 5,
-            1, 2, 4,
-            2, 3, 4
-        };
-
-        uv = new Vector2[]
-        {
-            new Vector2(0, 0.25f),
-            new Vector2(0, 0.75f),
-            new Vector2(0.5f, 1),
-            new Vector2(1, 0.75f),
-            new Vector2(1, 0.25f),
-            new Vector2(0.5f, 0),
-        };
-
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.uv = uv;
-        mesh.triangles = triangles;
-
-        gameObject.AddComponent<MeshFilter>();
-        GetComponent<MeshFilter>().mesh = mesh;
-
-        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
-        renderer.material.mainTexture = texture;
+        Mesh = new Mesh();
+        Mesh.vertices = Chunk.SharedMesh.vertices;
+        Mesh.uv = Chunk.SharedMesh.uv;
+        Mesh.triangles = Chunk.SharedMesh.triangles;
+        Mesh.RecalculateNormals();
     }
 }
